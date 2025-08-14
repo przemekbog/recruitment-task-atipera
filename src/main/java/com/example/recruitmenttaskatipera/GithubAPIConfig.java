86f6ee5618bbf24375dfbc1e6565179f834685ea
przemek_bog <42249946+przemekbog@users.noreply.github.com>
2025-08-14 14:09:05 +0200

    Added github config object

--- src/main/java/com/example/recruitmenttaskatipera/GithubAPIConfig.java
+++ src/main/java/com/example/recruitmenttaskatipera/GithubAPIConfig.java
@@ -1,4 +1,14 @@
 package com.example.recruitmenttaskatipera;
 
-public class GithubConfig {
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "app.github.api")
+public class GithubAPIConfig {
+    private String token;
 }
