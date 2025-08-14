b25b6373acef97fa86f00332f8990b8349918456
przemek_bog <42249946+przemekbog@users.noreply.github.com>
2025-08-14 21:11:27 +0200

    Added all args constructors to github DTOs

--- src/main/java/com/example/recruitmenttaskatipera/githubapi/dto/GithubRepositoryPartialDTO.java
+++ src/main/java/com/example/recruitmenttaskatipera/githubapi/dto/GithubRepositoryPartialDTO.java
@@ -1,11 +1,11 @@
 package com.example.recruitmenttaskatipera.githubapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GithubRepositoryPartialDTO {
     private String name;
