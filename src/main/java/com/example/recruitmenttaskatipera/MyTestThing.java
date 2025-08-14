75c815dd56b3ded9b09c4bc4f7118ccf0bca7b78
przemek_bog <42249946+przemekbog@users.noreply.github.com>
2025-08-14 13:10:06 +0200

    Initial commit

---
+++ src/main/java/com/example/recruitmenttaskatipera/MyTestThing.java
@@ -0,0 +1,4 @@
+package com.example.recruitmenttaskatipera;
+
+public class MyTestThing {
+}
---
+++ src/test/java/com/example/recruitmenttaskatipera/RecruitmentTaskAtiperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recruitmenttaskatipera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskAtiperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
