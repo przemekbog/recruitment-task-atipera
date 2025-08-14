ac48389151f01eeb1dd6fbb973e095b3b27c7cce
przemek_bog <42249946+przemekbog@users.noreply.github.com>
2025-08-14 21:11:03 +0200

    Added validating that github token was specified

--- src/main/java/com/example/recruitmenttaskatipera/githubapi/GithubAPIConfig.java
+++ src/main/java/com/example/recruitmenttaskatipera/githubapi/GithubAPIConfig.java
@@ -1,5 +1,6 @@
 package com.example.recruitmenttaskatipera.githubapi;
 
+import jakarta.annotation.PostConstruct;
 import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -10,4 +11,11 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "app.github.api")
 public class GithubAPIConfig {
     private String token;
+
+    @PostConstruct
+    public void validate() {
+        if (token == null || token.isBlank()) {
+            throw new IllegalStateException("Github token is required but not set");
+        }
+    }
 }
